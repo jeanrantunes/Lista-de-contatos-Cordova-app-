7053dea39ce383e1cfc81b737eea27ed1348261e
jeanrantunes <jeanrantunes93@gmail.com>
2016-09-27 01:31:39 -0300

    Lista de contatos 1.0

--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,29 @@
-
 document.addEventListener("deviceready", onDeviceReady, false);
+
 function onDeviceReady() {
-//alert(cordova.file.externalRootDirectory);
-    
-    //listPath(myPath);
-    window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory + "list.json", gotFile, fail);
-     
-    
-    
+    //alert(cordova.file.externalDataDirectory);
+    var listContacts = new ListContacts(cordova.file.externalRootDirectory+"Contacts/list.json");
+    listContacts.getListContacts();
 }
+/*$(document).ready(function(){
+    var listContacts = new ListContacts("Contacts/list.json");
+    listContacts.getListContacts();  
+});*/
+
+/*
 function fail(e) {
     console.log("FileSystem Error");
     console.dir(e);
 }
 
 function gotFile(fileEntry) {
-    $data = "";
     fileEntry.file(function(file) {
         var reader = new FileReader();
 
-        reader.onloadend = function(e) {
-           
-            
+        reader.onloadend = function(e) {   
             var parsedData = JSON.parse(this.result);
             alert(parsedData[0].Email);
         }
-
         reader.readAsText(file);
-
     });
-
-
-}
+}*/
